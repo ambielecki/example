b31ea1f0e2e952f09c65f9de146651f143e1eaae
Andrew Bielecki <ambielecki@gmail.com>
2016-07-08 19:35:48 -0400

    Add meta en

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <title>@yield('title', 'Application Title')</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta lang="en">
     {{--<link rel='icon' href='/images/favicon.ico'>--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
