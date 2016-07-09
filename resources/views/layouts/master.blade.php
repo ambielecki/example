ed40d90c50d9af03a6e166303ba107ff8bf49d60
Andrew Bielecki <ambielecki@gmail.com>
2016-07-09 12:25:59 -0400

    fixed html lang

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>@yield('title', 'Application Title')</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <meta lang="en">
     {{--<link rel='icon' href='/images/favicon.ico'>--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/f3941c2d61.js"></script>
